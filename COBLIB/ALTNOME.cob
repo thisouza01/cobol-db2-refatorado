4983cf4d8af84c60e89ccf9ad79c07ecdaacee9d
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-10-07 07:48:00 -0300

    Add: Subprograma que altera nome do funcionário.
    Call aninhado.

---
+++ COBLIB/ALTNOME.cob
@@ -0,0 +1,53 @@
+       IDENTIFICATION                DIVISION.
+       PROGRAM-ID.                   ALTNOME.
+      ************************************************
+      * PROGRAMA DE ALTERACAO DO NOME DO FUNCIONARIO *
+      ************************************************
+       ENVIRONMENT                   DIVISION.
+       CONFIGURATION                 SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+      *
+       DATA                          DIVISION.
+       LINKAGE                       SECTION.
+       01 LK-NOMEFUN-ACCEPT          PIC X(30).
+       01 LK-CODFUN                  PIC X(04).
+      *
+       WORKING-STORAGE               SECTION.
+           EXEC SQL
+               INCLUDE SQLCA
+           END-EXEC.
+           EXEC SQL
+               INCLUDE BOOKFUNC
+           END-EXEC.
+      *
+       77 RETORNO-SQLCODE            PIC -999   VALUE ZEROS.
+      *
+       PROCEDURE                     DIVISION.
+      *
+       PERFORM ALTERA-NOME.
+       GOBACK.
+      *
+       ALTERA-NOME.
+           MOVE LK-NOMEFUN-ACCEPT TO DB2-NOMEFUN-TEXT.
+           CALL "CONTNOME" USING DB2-NOMEFUN.
+           EXEC SQL
+               UPDATE IBMUSER.FUNCIONARIOS
+               SET NOMEFUN = :DB2-NOMEFUN
+                     WHERE CODFUN = :LK-CODFUN
+           END-EXEC.
+           EVALUATE SQLCODE
+           WHEN 0
+              DISPLAY 'NOME DO FUNCIONARIO ' LK-CODFUN
+                      ' FOI ALTERADO PARA ' DB2-NOMEFUN-TEXT
+           WHEN 100
+              DISPLAY 'FUNCIONARIO ' LK-CODFUN
+                      ' NAO EXISTE'
+           WHEN OTHER
+              MOVE SQLCODE TO RETORNO-SQLCODE
+              DISPLAY 'ERRO ' RETORNO-SQLCODE
+                      ' NO COMANDO UPDATE DO NOME'
+              MOVE 12 TO RETURN-CODE
+              GOBACK.
+           END-EVALUATE.
+      
