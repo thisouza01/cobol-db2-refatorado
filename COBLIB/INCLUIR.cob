131ea02c5374c1d2e80e30e257ea48ad21539b82
Thiago <tihsouza01@gmail.com>
2025-10-14 10:55:37 -0300

    Add: Programa de inclusão do funcionário com lógica de processamento e validação.

---
+++ COBLIB/INCLUIR.cob
@@ -0,0 +1,72 @@
+       IDENTIFICATION                DIVISION.
+       PROGRAM-ID.                   INCLUIR.
+      ****************************************
+      * PROGRAMA DE INCLUSAO DO FUNCIONARIO  *
+      ****************************************
+       ENVIRONMENT                   DIVISION.
+       CONFIGURATION                 SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+      *
+       DATA                          DIVISION.
+      *
+       WORKING-STORAGE               SECTION.
+      *
+       LINKAGE                       SECTION.
+       01  LK-ACCEPT.
+              05 LK-FUNCAO-ACCEPT       PIC X.
+              05 LK-CODFUN-ACCEPT       PIC X(4).
+              05 LK-NOMEFUN-ACCEPT      PIC X(30).
+              05 LK-SALARIOFUN-ACCEPT   PIC 9(6)V99.
+              05 LK-DEPTOFUN-ACCEPT     PIC X(3).
+              05 LK-ADMISSFUN-ACCEPT    PIC X(11).
+              05 LK-IDADEFUN-ACCEPT     PIC 99.
+       77   LK-EMAILFUN-ACCEPT          PIC X(30).       
+      *
+       PROCEDURE                     DIVISION USING LK-ACCEPT,
+                                                    LK-EMAILFUN-ACCEPT.
+      *
+       PERFORM INCLUI-FUNCIONARIO.
+       GOBACK.
+      *
+       INCLUI-FUNCIONARIO.
+          MOVE LK-CODFUN-ACCEPT     TO DB2-CODFUN.
+          MOVE LK-NOMEFUN-ACCEPT    TO DB2-NOMEFUN-TEXT.
+      *   Conta quantidade de caracteres e atualiza DB2-NOMEFUN-LEN.
+          CALL "CONTNOME"           USING DB2-NOMEFUN.
+          MOVE LK-SALARIOFUN-ACCEPT TO DB2-SALARIOFUN.
+          MOVE LK-DEPTOFUN-ACCEPT   TO DB2-DEPTOFUN.
+          MOVE LK-ADMISSFUN-ACCEPT  TO DB2-ADMISSFUN.
+          MOVE LK-IDADEFUN-ACCEPT   TO DB2-IDADEFUN.
+          MOVE LK-EMAILFUN-ACCEPT   TO DB2-EMAILFUN-TEXT.
+      *   Conta quantidade de caracteres e atualiza DB2-EMAILFUN-LEN.
+          CALL "CONTMAIL"           USING DB2-EMAILFUN.
+
+          EXEC SQL
+             INSERT INTO EAD719.FUNCIONARIOS
+             VALUES(  :DB2-CODFUN,
+                      :DB2-NOMEFUN,
+                      :DB2-SALARIOFUN,
+                      :DB2-DEPTOFUN,
+                      :DB2-ADMISSFUN,
+                      :DB2-IDADEFUN,
+                      :DB2-EMAILFUN)
+          END-EXEC.
+
+          EVALUATE SQLCODE
+             WHEN 0
+                   DISPLAY 'FUNCIONARIO ' DB2-CODFUN
+                         ' FOI INCLUIDO!'
+             WHEN -803
+                   DISPLAY 'FUNCIONARIO ' DB2-CODFUN
+                         ' JA EXISTE!'
+             WHEN -530
+                   DISPLAY 'DEPARTAMENTO ' DB2-DEPTOFUN
+                         ' NAO EXISTE!'
+             WHEN OTHER
+                   MOVE SQLCODE TO WK-SQLCODE-EDIT
+                   DISPLAY 'ERRO ' WK-SQLCODE-EDIT
+                         ' NO COMANDO INSERT'
+                   MOVE 12 TO RETURN-CODE
+                   STOP RUN
+          END-EVALUATE.       
